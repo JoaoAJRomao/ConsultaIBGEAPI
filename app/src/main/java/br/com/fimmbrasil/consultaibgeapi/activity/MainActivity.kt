ce63763e1371d9102dd77085f1c4ed9c76fe8b75
João Arthur <tupi_1800@hotmail.com>
2019-06-21 09:56:28 -0300

    Trocando para TableLayout
    Aplicativo já usa TableLayout para apresentar informações.

--- app/src/main/java/br/com/fimmbrasil/consultaibgeapi/activity/MainActivity.kt
+++ app/src/main/java/br/com/fimmbrasil/consultaibgeapi/activity/MainActivity.kt
@@ -2,37 +2,33 @@ package br.com.fimmbrasil.consultaibgeapi.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.widget.*
 import br.com.fimmbrasil.consultaibgeapi.R
-import br.com.fimmbrasil.consultaibgeapi.adapter.UFAdapter
 import br.com.fimmbrasil.consultaibgeapi.models.UF
 import br.com.fimmbrasil.consultaibgeapi.service.IBGEService
 import br.com.fimmbrasil.consultaibgeapi.service.RetrofitConfig
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity(){
     private lateinit var ibgeService: IBGEService
-    private lateinit var recyclerView: RecyclerView
     private lateinit var spinner: Spinner
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ConfiguracaoInicial()
+        ibgeService = RetrofitConfig.getIBGEService()
+        spinner = findViewById(R.id.main_spinner)
 
         var unidadesFederativas = arrayOf("Rondônia","Acre","Amazonas","Roraima","Pará","Amapá","Tocantins","Maranhão","Piauí","Ceará","Rio Grande do Norte","Paraíba","Pernambuco","Alagoas","Sergipe","Bahia","Minas Gerais","Espírito Santo","Rio de Janeiro","São Paulo","Paraná","Santa Catarina","Rio Grande do Sul","Mato Grosso do Sul","Mato Grosso","Goiás","Distrito Federal")
 
-
         if(spinner!=null){
-            val arrayAdapterUnidadeFederativa = ArrayAdapter(this,android.R.layout.simple_spinner_item, unidadesFederativas)
+            val arrayAdapterUnidadeFederativa = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, unidadesFederativas) //R.layout.simple_spinner_dropdown_item -> da espaço entre cada item na lista
             spinner.adapter = arrayAdapterUnidadeFederativa
 
             spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
@@ -42,7 +38,6 @@ class MainActivity : AppCompatActivity(){
 
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                     Toast.makeText(applicationContext,unidadesFederativas[position],Toast.LENGTH_SHORT).show()
-                    var valor = converte(unidadesFederativas[position])
                     var uf = ibgeService.getMesorregiaoById(converte(unidadesFederativas[position]))
                     uf.enqueue(object :Callback<UF?>{
                         override fun onFailure(call: Call<UF?>, t: Throwable) {
@@ -50,20 +45,16 @@ class MainActivity : AppCompatActivity(){
                         }
 
                         override fun onResponse(call: Call<UF?>, response: Response<UF?>) {
-                            recyclerView.adapter = response.body()?.let { UFAdapter(applicationContext, it) }
+                            main_id.text=response.body()?.id.toString()
+                            main_nome.text=response.body()?.nome
+                            main_sigla.text=response.body()?.sigla
+                            main_regiao.text=response.body()?.regiao?.nome
                         }
                     })
                 }
 
             }
         }
-    }
-
-    fun ConfiguracaoInicial() {
-        ibgeService = RetrofitConfig.getIBGEService()
-        recyclerView = findViewById(R.id.main_recyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
-        spinner = findViewById(R.id.main_spinner)
     }
 
     fun converte(texto:String):Int{
